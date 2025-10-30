86eee7897ca5d573788ebc974b5766d802792c45
Kartikeya Mishra <71225085+thekartikeyamishra@users.noreply.github.com>
2025-10-30 12:07:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
-import './index.css';
-import App from './App.jsx';
+// File: src/main.jsx
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { setupGlobalErrorHandlers } from './utils/errorHandler.js';
+
+// Setup error handlers
+setupGlobalErrorHandlers();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
